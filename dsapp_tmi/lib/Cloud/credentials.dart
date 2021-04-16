ff535a2c64409f71bf63c57356ec71cba6974392
Dr. Frank-N-Furter <74541082+dr-frankenfurter@users.noreply.github.com>
2021-04-16 20:04:20 +0200

    Clase para el acceso a la API en el cloud

---
+++ dsapp_tmi/lib/Cloud/credentials.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/services.dart';
+import 'package:googleapis/vision/v1.dart';
+import 'package:googleapis_auth/auth_io.dart';
+import 'package:gcloud/storage.dart';
+
+class CredentialsProvider {
+  CredentialsProvider();
+
+  Future<ServiceAccountCredentials> get _credentialsvision async {
+    String _filevision = await rootBundle.loadString('assets/maenstorage.json');
+    return ServiceAccountCredentials.fromJson(_filevision);
+  }
+
+  Future<ServiceAccountCredentials> get _credentialsstorage async {
+    String _filestorage =
+        await rootBundle.loadString('assets/maenstorage.json');
+    return ServiceAccountCredentials.fromJson(_filestorage);
+  }
+
+  Future<AutoRefreshingAuthClient> get clientvision async {
+    AutoRefreshingAuthClient _client = await clientViaServiceAccount(
+        await _credentialsvision, [VisionApi.CloudVisionScope]).then((c) => c);
+    return _client;
+  }
+
+  Future<AutoRefreshingAuthClient> get clientstorage async {
+    AutoRefreshingAuthClient _client =
+        await clientViaServiceAccount(await _credentialsstorage, Storage.SCOPES)
+            .then((c) => c);
+    return _client;
+  }
+}
