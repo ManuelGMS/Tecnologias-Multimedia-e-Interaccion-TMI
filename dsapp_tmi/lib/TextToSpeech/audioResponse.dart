5ab8ca88d3f924e16c4e66139c02b16921696319
alberc01 <44811193+alberc01@users.noreply.github.com>
2021-05-17 18:26:05 +0200

    Añadir comentarios en audioResponse.dart

--- dsapp_tmi/lib/TextToSpeech/audioResponse.dart
+++ dsapp_tmi/lib/TextToSpeech/audioResponse.dart
@@ -1,8 +1,11 @@
 class AudioResponse {
+  // Contenido que sera reproducido por la API de Google.
   final String audioContent;
 
+  // Constructora AudioResponse
   AudioResponse(this.audioContent);
 
+  // Constructora AudioResponse para convertir json a audioContent
   AudioResponse.fromJson(Map<String, dynamic> json)
       : audioContent = json['audioContent'];
 }
