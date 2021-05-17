adacf5e3662cda947ead5a978961289454b202f2
enriquemaestro <emaestro@ucm.es>
2021-05-17 17:52:45 +0200

    Refactor de comenzar.dart

--- dsapp_tmi/lib/Menu/comenzar.dart
+++ dsapp_tmi/lib/Menu/comenzar.dart
@@ -20,10 +20,10 @@ class _MyComenzar extends State<MyComenzar> {
   // Lista de elementos reconocidos en el frame de vídeo.
   List<dynamic> _recognitions;
   CameraImage _frame;
-  String _OCRtext; // aqui estan las valiosas palabras
-  CloudOCR apiOcr;
-  bool isUploaded = false;
-  bool loading = false;
+  String _OCRtext; // Texto para el OCR
+  CloudOCR apiOcr; // Servicio para el OCR
+  bool isUploaded = false; // Atributo para controlar el estado del OCR
+  bool loading = false; // Atributo para controlar el estado del OCR
 
   @override
   void initState() {
@@ -33,7 +33,7 @@ class _MyComenzar extends State<MyComenzar> {
     */
     super.initState();
 
-    // Arranca la conexion con el gogle on the line de la nube
+    // Arranca la conexion con Google para el OCR
     apiOcr = CloudOCR();
 
     // Carga el modelo de red neuronal y las etiquetas de cada clase.
@@ -42,7 +42,7 @@ class _MyComenzar extends State<MyComenzar> {
         labels: "assets/ssd_mobilenet.txt");
   }
 
-  // Método para actualizar la pantalla y los datos asociados a los objetos reconocidos.
+  // Método para actualizar la pantalla y los objetos reconocidos.
   _recognitionsCB(List<dynamic> recognitions) {
     /*
     La llamada a setState le dice al framework Flutter que algo ha cambiado en
@@ -58,35 +58,24 @@ class _MyComenzar extends State<MyComenzar> {
     setState(() {
       // Se comprueba que el mapa que le llega tenga alguna imagen reconocida
       if(recognitions[0].containsKey("detectedClass")){
-        var ttsService =  TextToSpeechService(Spach.ttsAPIKEY);
-        String texto = recognitions[0]["detectedClass"];// Se saca el valor de la palabra
-        ttsService.textToSpeech(text:texto); // Se llama a la api
+        var ttsService =  TextToSpeechService(Spach.ttsAPIKEY); // Obtiene el servicio para el TTS
+        String texto = recognitions[0]["detectedClass"]; // Obtenemos el valor de la palabra
+        ttsService.textToSpeech(text:texto); // Llamada a la API
       }
     });
   }
 
-  // Método dedicado a oscar y aiko los mejores joder. Sirve para guardar el frame
-  // del OCR
+  // Para guardar el frame del OCR
   _frameOCR(CameraImage oscar) {
     setState(() {
       this._frame = oscar;
     });
   }
 
-  // @override
-  // Widget build(BuildContext context) => FutureBuilder(
-  //       initialData: false,
-  //       future: apiOcr.ocr(""),
-  //       builder: (context, snapshot) =>
-  //           snapshot.hasData ? _buildWidget(snapshot.data) : const SizedBox(),
-  //     );
-
   @override
   Widget build(BuildContext context) {
     /* Pasamos un CallBack a la cámara para que podamos recuperar a
       través de el la lista de Bounding Boxes. */
-    Camera cam; // = Camera(this.widget._systemCamera, _recognitionsCB);
-    // peta
     return Scaffold(
         appBar: AppBar(
           title: Text('Comenzar'),
@@ -99,7 +88,7 @@ class _MyComenzar extends State<MyComenzar> {
             children: [
               // Muestra lo que devuelve su método "build" (el vídeo grabado).
               // Obtiene la cámara a través del widget asociado este estado.
-              cam = Camera(this.widget._systemCamera, _recognitionsCB, _frameOCR, _recogttsCB),
+              Camera(this.widget._systemCamera, _recognitionsCB, _frameOCR, _recogttsCB),
               if (loading)
                 Center(
                   child: CircularProgressIndicator(),
@@ -116,26 +105,15 @@ class _MyComenzar extends State<MyComenzar> {
                         fontSize: 50.0,
                         backgroundColor: Colors.black),
                   ),
-                  // new Padding(padding: EdgeInsets.only(top: 1.0)),
-                  // new CircleAvatar(
-                  //   radius: 20,
-                  //   backgroundColor: Colors.green,
-                  //   child: Icon(
-                  //     Icons.check,
-                  //     color: Colors.white,
-                  //     size: 20,
-                  //   ),
-                  // ),
                 ])),
               Align(
                 alignment: Alignment.bottomCenter,
                 child: FutureBuilder(
-                    //future: getSwitch('my_vibracion_key'),
                     initialData: false,
                     builder: (context, snapshot) {
                       return FloatingActionButton(
                         child: Icon(
-                            Icons.chrome_reader_mode), // Buscar un icono mejor
+                            Icons.chrome_reader_mode),
                         tooltip: 'OCR',
                         // camara modificada para que haga fotos
                         onPressed: _processImage,
@@ -153,7 +131,6 @@ class _MyComenzar extends State<MyComenzar> {
               BoundingBox(_recognitions == null ? [] : _recognitions)
             ],
           ))
-          // Los rumores de la separacion de golfday desmentidos
         ]));
   }
 
